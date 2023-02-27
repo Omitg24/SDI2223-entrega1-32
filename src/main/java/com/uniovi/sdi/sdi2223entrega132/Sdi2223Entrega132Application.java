b9834112d1cdc005990394f656883d3fa8ad4558
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-02-27 20:08:52 +0100

    27/02/2023 - Omar - Creación del proyecto

---
+++ src/main/java/com/uniovi/sdi/sdi2223entrega132/Sdi2223Entrega132Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2223entrega132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223Entrega132Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223Entrega132Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi/sdi2223entrega132/Sdi2223Entrega132ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2223entrega132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223Entrega132ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
